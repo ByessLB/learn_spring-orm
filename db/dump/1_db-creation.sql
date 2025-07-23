fa36b879e0f0881b546b4098b1edcd60e0c6de43
MauiLBDev <maui.lb.dev@gmail.com>
2025-07-23 17:01:36 +0200

    DTO:OK, Repositories:OK, entities:OK

--- db/dump/1_db-creation.sql
+++ db/dump/1_db-creation.sql
@@ -1,14 +1,22 @@
 
+DROP TABLE if exists public.client CASCADE;
 DROP TABLE if exists public.account CASCADE;
 
 /* TODO : ajouter la création de la table "client" */
 
-CREATE TABLE account (
-	id SERIAL PRIMARY KEY,
+CREATE TABLE client (
+	id UUID PRIMARY KEY,
 	first_name VARCHAR(100),
 	last_name VARCHAR(50),
-	email VARCHAR(50),
-	birthday date,
-	creationTime timestamp,
-	balance bigint
+	birthdate DATE,
+	email VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE account (
+	id SERIAL PRIMARY KEY,
+	creationTime TIMESTAMP,
+	balance NUMERIC(25,2),
+	active BOOLEAN,
+	client_id UUID,
+	FOREIGN KEY (client_id) REFERENCES client(id)
 );
